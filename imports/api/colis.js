86156208db8a8803a923741b7a4096c78959db2b
finexs.voyages <finexs-informatique@finexsvoyages.net>
2017-06-16 17:20:02 +0100

    mise a jour envoie sms

--- imports/api/colis.js
+++ imports/api/colis.js
@@ -12,168 +12,169 @@ if (Meteor.isServer) {
     Meteor.publish('colis', function () {
         return Colis.find();
     });
-}
 
-Meteor.methods({
-    'colis.insert'(code, bus, dest , amount, nameExp, telExp,nameDest, telDest, desc  ) {
+    Meteor.methods({
+        'colis.insert'(code, bus, dest , amount, nameExp, telExp,nameDest, telDest, desc  ) {
 
-        new SimpleSchema({
-            code: {
-                type: String,
-                label: 'Code',
-            },
-            bus: {
-                type: String,
-                label: 'Immatriculation',
-            },
-            dest: {
-                type: String,
-                label: 'Destination',
-                allowedValues: ['YAOUNDE','DOUALA'],
-            },
-            amount: {
-                type: SimpleSchema.Integer,
-                label: 'The amount',
-            },
-            nameExp: {
-                type: String,
-                label: 'Name exp.',
-            },
-            telExp: {
-                type: String,
-                label: 'Tel. exp.',
-                regEx: SimpleSchema.RegEx.Phone,
-                min : 12,
-                max : 12
-            },
-            nameDest: {
-                type: String,
-                label: 'Name dest.',
-            },
-            telDest: {
-                type: String,
-                label: 'Tel dest.',
-                regEx: SimpleSchema.RegEx.Phone,
-                min : 12,
-                max : 12
-            },
-            desc: {
-                type: String,
-                label: 'Desc. pack.',
-                min : 3,
-                max : 70,
-            }
+            new SimpleSchema({
+                code: {
+                    type: String,
+                    label: 'Code',
+                },
+                bus: {
+                    type: String,
+                    label: 'Immatriculation',
+                },
+                dest: {
+                    type: String,
+                    label: 'Destination',
+                    allowedValues: ['YAOUNDE','DOUALA'],
+                },
+                amount: {
+                    type: SimpleSchema.Integer,
+                    label: 'The amount',
+                },
+                nameExp: {
+                    type: String,
+                    label: 'Name exp.',
+                },
+                telExp: {
+                    type: String,
+                    label: 'Tel. exp.',
+                    regEx: SimpleSchema.RegEx.Phone,
+                    min : 12,
+                    max : 12
+                },
+                nameDest: {
+                    type: String,
+                    label: 'Name dest.',
+                },
+                telDest: {
+                    type: String,
+                    label: 'Tel dest.',
+                    regEx: SimpleSchema.RegEx.Phone,
+                    min : 12,
+                    max : 12
+                },
+                desc: {
+                    type: String,
+                    label: 'Desc. pack.',
+                    min : 3,
+                    max : 70,
+                }
 
-        }).validate({ code, bus, dest , amount, nameExp, telExp,nameDest, telDest, desc   });
+            }).validate({ code, bus, dest , amount, nameExp, telExp,nameDest, telDest, desc   });
 
-        Colis.insert({
-            code,
-            bus,
-            dest,
-            amount,
-            nameExp,
-            telExp,
-            nameDest,
-            telDest,
-            desc,
-            state : 'A',
-            visible : true ,
-            updatedAt : new Date().getTime(),
-            DateTimeExp : new Date().getTime(),
-            expUserId : this.userId
-        }, (err)=>{ if (!err)  {
-            if ( Meteor.isServer ) {
-                const sender = 'FINEXS VOYAGES' ;
-                const message = nameDest+', votre colis ('+code+') est en route ,bus '+bus+'. Si apres 05 ou 06h de temps vous avez pas recu de retour appelez le 697509899'
-                //console.log( telDest+'   '+message );
-                request('http://api.vassarl.com:9501/api?action=sendmessage&username=FINEXS&password=Finexs12345&originator='+sender+'&recipient='+telDest+'&messagetype=SMS:TEXT&messagedata='+message, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        //console.log( error )
-                    }
-                })
-            }
-        } } );
-
-
-    },
-    'colis.arrived'(_id, code, dest, nameDest, telDest ) {
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
-
-        new SimpleSchema({
-            _id: {
-                type: String,
-                min: 1
-            }
-        }).validate({ _id});
-
-        Colis.update({
-            _id
-        }, {
-            $set: {
-                state : 'B',
+            Colis.insert({
+                code,
+                bus,
+                dest,
+                amount,
+                nameExp,
+                telExp,
+                nameDest,
+                telDest,
+                desc,
+                state : 'A',
+                visible : true ,
                 updatedAt : new Date().getTime(),
-                DateTimeArr : new Date().getTime(),
-                userIdArr : this.userId
+                DateTimeExp : new Date().getTime(),
+                expUserId : this.userId
+            }, (err)=>{ if (!err)  {
+                if ( Meteor.isServer ) {
+                    const sender = 'FINEXS VOYAGES' ;
+                    const message = nameDest+', votre colis ('+code+') est en route ,bus '+bus+'. Si apres 05 ou 06h de temps vous avez pas recu de retour appelez le 697509899'
+                    //console.log( telDest+'   '+message );
+                    request('http://api.vassarl.com:9501/api?action=sendmessage&username=FINEXS&password=Finexs12345&originator='+sender+'&recipient='+telDest+'&messagetype=SMS:TEXT&messagedata='+message, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            //console.log( error )
+                        }
+                    })
+                }
+            } } );
+
+
+        },
+        'colis.arrived'(_id, code, dest, nameDest, telDest ) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
             }
-        },(err)=>{ if (!err) {
-            if ( Meteor.isServer ) {
+
+            new SimpleSchema({
+                _id: {
+                    type: String,
+                    min: 1
+                }
+            }).validate({ _id});
+
+            Colis.update({
+                _id
+            }, {
+                $set: {
+                    state : 'B',
+                    updatedAt : new Date().getTime(),
+                    DateTimeArr : new Date().getTime(),
+                    userIdArr : this.userId
+                }
+            },(err)=>{ if (!err) {
                 const sender = 'FINEXS VOYAGES' ;
                 const message = nameDest+', votre colis ('+code+') est arrive a '+dest+',vous pouvez passer le recuperer. Finexs Voyages vous remercie de votre confiance.'
                 console.log( telDest+'   '+message );
                 request('http://api.vassarl.com:9501/api?action=sendmessage&username=FINEXS&password=Finexs12345&originator='+sender+'&recipient='+telDest+'&messagetype=SMS:TEXT&messagedata='+message, function (error, response, body) {
                     if (!error && response.statusCode == 200) { console.log( error ) }
                 })
-            }
-        }} );
 
-    },
-    'colis.end'(_id ) {
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
+            }} );
+
+        },
+        'colis.end'(_id ) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            new SimpleSchema({
+                _id: {
+                    type: String,
+                    min: 1
+                }
+            }).validate({ _id });
+
+            Colis.update({
+                _id
+            }, {
+                $set: {
+                    state : 'C',
+                    updatedAt : new Date().getTime(),
+                    DateTimeEnd : new Date().getTime(),
+                    userIdEnd : this.userId
+                }
+            });
+
+        },
+        'colis.deleted'(_id ) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            new SimpleSchema({
+                _id: {
+                    type: String,
+                    min: 1
+                }
+            }).validate({ _id });
+
+            Colis.update({
+                _id
+            }, {
+                $set: {
+                    deletedAt : new Date().getTime(),
+                    visible: false,
+                    userIdDeleted : this.userId
+                }
+            });
         }
 
-        new SimpleSchema({
-            _id: {
-                type: String,
-                min: 1
-            }
-        }).validate({ _id });
+    });
+}
 
-        Colis.update({
-            _id
-        }, {
-            $set: {
-                state : 'C',
-                updatedAt : new Date().getTime(),
-                DateTimeEnd : new Date().getTime(),
-                userIdEnd : this.userId
-            }
-        });
 
-    },
-    'colis.deleted'(_id ) {
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
-
-        new SimpleSchema({
-            _id: {
-                type: String,
-                min: 1
-            }
-        }).validate({ _id });
-
-        Colis.update({
-            _id
-        }, {
-            $set: {
-                deletedAt : new Date().getTime(),
-                visible: false,
-                userIdDeleted : this.userId
-            }
-        });
-    }
-
-});
--- imports/api/resas.js
+++ imports/api/resas.js
@@ -11,95 +11,96 @@ if ( Meteor.isServer ) {
     Meteor.publish('resas', function() {
         return Resas.find({});
     });
-}
 
-Meteor.methods({
-    'resas.insert'(resa , cni , phone , name , desc){
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
+    Meteor.methods({
+        'resas.insert'(resa , cni , phone , name , desc){
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
 
-        try {
-            new SimpleSchema({
-                resa: {
-                    type: Date,
-                    label: 'Date',
-                },
-                cni: {
-                    type: String,
-                    label: 'CNI',
-                },
-                phone: {
-                    type: String,
-                    label: 'Tel dest.',
-                    regEx: SimpleSchema.RegEx.Phone,
-                    min : 12,
-                    max : 12
-                },
-                name: {
-                    type: String,
-                    label: 'Nom',
-                },
-                desc: {
-                    type: String,
-                    label: 'Desc. pack.',
-                    min : 3,
-                    max : 70,
-                }
-            }).validate({resa , cni , phone , name , desc});
-        } catch (e) {
-            throw new Meteor.Error(400, e.message);
-        }
+            try {
+                new SimpleSchema({
+                    resa: {
+                        type: Date,
+                        label: 'Date',
+                    },
+                    cni: {
+                        type: String,
+                        label: 'CNI',
+                    },
+                    phone: {
+                        type: String,
+                        label: 'Tel dest.',
+                        regEx: SimpleSchema.RegEx.Phone,
+                        min : 12,
+                        max : 12
+                    },
+                    name: {
+                        type: String,
+                        label: 'Nom',
+                    },
+                    desc: {
+                        type: String,
+                        label: 'Desc. pack.',
+                        min : 3,
+                        max : 70,
+                    }
+                }).validate({resa , cni , phone , name , desc});
+            } catch (e) {
+                throw new Meteor.Error(400, e.message);
+            }
 
-        Resas.insert({
-            resa : resa,
-            cni,
-            phone,
-            name,
-            desc,
-            userId: this.userId,
-            visible: true
-        } , (err)=>{ if (!err)  {
-            if ( Meteor.isServer ) {
+            Resas.insert({
+                resa : resa,
+                cni,
+                phone,
+                name,
+                desc,
+                userId: this.userId,
+                visible: true,
+                paid : false,
+            } , (err)=>{ if (!err)  {
                 const sender = 'FINEXS VOYAGES' ;
                 const sms = `Hi ${name}, Resa pour ${ moment(resa).locale("fr").format("dddd, lll")} ; Expire 30min avant l'heure de la resa. Tels : 699998636/696943131/691819290/691817924`;
                 //const message = nameDest+', votre colis ('+code+') est en route ,bus '+bus+'. Si apres 05 ou 06h de temps vous avez pas recu de retour appelez le 697509899';
                 request('http://api.vassarl.com:9501/api?action=sendmessage&username=FINEXS&password=Finexs12345&originator='+sender+'&recipient='+phone+'&messagetype=SMS:TEXT&messagedata='+sms, function (error, response, body) {
                     if (!error && response.statusCode == 200) { }
                 })
+
             }
+
+            });
+        },
+        'resas.confirmation' (_id, visible) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+            Resas.update( _id, { $set: { paid : visible } } );
+        },
+        'resa.deleted'(_id ) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            new SimpleSchema({
+                _id: {
+                    type: String,
+                    min: 1
+                }
+            }).validate({ _id });
+
+            Resas.update({
+                _id
+            }, {
+                $set: {
+                    deletedAt : new Date().getTime(),
+                    visible: false,
+                    userIdDeleted : this.userId
+                }
+            });
         }
 
-        });
-    },
-    'resas.confirmation' (_id, visible) {
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
-        Resas.update( _id, { $set: { visible } } );
-    },
-    'resa.deleted'(_id ) {
-        if (!this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
 
-        new SimpleSchema({
-            _id: {
-                type: String,
-                min: 1
-            }
-        }).validate({ _id });
+    });
+}
 
-        Resas.update({
-            _id
-        }, {
-            $set: {
-                deletedAt : new Date().getTime(),
-                visible: false,
-                userIdDeleted : this.userId
-            }
-        });
-    }
-
-
-});
