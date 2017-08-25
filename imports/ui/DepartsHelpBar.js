8e20e8c8d63b2e0a5167e4eaaf71d48d59c6a541
finexs.voyages <finexs-informatique@finexsvoyages.net>
2017-08-25 11:35:03 +0100

    version 1.0.2

--- imports/ui/DepartsHelpBar.js
+++ imports/ui/DepartsHelpBar.js
@@ -23,6 +23,13 @@ export class DepartsHelpBar extends React.Component{
         console.log(nextProps)
 
     }
+    sendSMS(message){
+        const changeState = confirm("Rapport a envoyer : \n"+ message);
+        if (changeState) {
+            Meteor.call('nkSMS',message,function (err) {
+                if (!err) { console.log('well done')} else { console.log(err)} } )
+        }
+    }
     render(){
         const  sms = this.state.departs ? `Ttal: ${filtrage(this.state.departs).length} vges -> ${sommes(filtrage(this.state.departs))}Fcfa
 TtalVIP: ${filtrage(this.state.departs,undefined,'VIP').length} vges -> ${sommes(filtrage(this.state.departs,undefined,'VIP'))}Fcfa
@@ -38,8 +45,7 @@ YdeCla: ${filtrage(this.state.departs,'Yaounde','classique').length} vges -> ${s
                 fluid
                 animated='fade'
                 color='green'
-                onClick={ ()=>{sms ? Meteor.call('nkSMS',sms,function (err) {
-                    if (!err) { console.log('well done')} else { console.log(err)} } ): console.log('Error') } }>
+                onClick={ ()=>{sms ? this.sendSMS(sms) : console.log('Error') } }>
                 <Button.Content visible>
                     {this.state.departs ? this.state.departs.length : '0'} elts ->
                     Total : {this.state.departs ? sommes(this.state.departs): '0'} Fcfa
